f846fd969f14c984137ec85007a54773a66f3800
Shawn Sprehe <memakeit@gmail.com>
2010-12-04 15:05:34 -0600

    updated config

--- config/mmi-cache.php
+++ config/mmi-cache.php
@@ -7,11 +7,11 @@ return array
 	'lifetimes' => array
 	(
 		MMI_Cache::CACHE_TYPE_DEFAULT	=> 8 * Date::HOUR,
-		MMI_Cache::CACHE_TYPE_CSS		=> Date::YEAR,
+		MMI_Cache::CACHE_TYPE_CSS		=> 1 * Date::YEAR,
 		MMI_Cache::CACHE_TYPE_DATA		=> 15 * Date::MINUTE,
 		MMI_Cache::CACHE_TYPE_FEED		=> 15 * Date::MINUTE,
 		MMI_Cache::CACHE_TYPE_FRAGMENT	=> 4 * Date::HOUR,
-		MMI_Cache::CACHE_TYPE_JS		=> Date::YEAR,
+		MMI_Cache::CACHE_TYPE_JS		=> 1 * Date::YEAR,
 		MMI_Cache::CACHE_TYPE_PAGE		=> 15 * Date::MINUTE,
 		MMI_Cache::CACHE_TYPE_SYSTEM	=> 1 * Date::HOUR,
 		MMI_Cache::CACHE_TYPE_XML		=> 15 * Date::MINUTE,
